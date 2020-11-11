8f066d11039bacd2fcf7b8df58b38560d26e2548
robinsondaniel989 <45187231+robinsondaniel989@users.noreply.github.com>
2020-11-11 12:58:21 -0500

    header update

--- Chess.h
+++ Chess.h
@@ -15,6 +15,7 @@ public:
 	void printBoard(vector<vector<Chess>>);
 	vector<vector<Chess>> movePiece(vector<vector<Chess>> chessBoard, int player);
 	bool clearPath(vector<vector<Chess>> chessBoard, int y, int x, int y2, int x2);
+	bool checkMate();
 
 };
 
